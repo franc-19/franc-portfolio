4369f996d5f8df3d5dc6bc82fd719c64c2816030
franc-19 <133057947+franc-19@users.noreply.github.com>
2026-06-22 11:28:19 +0300

    updates

---
+++ script.js
@@ -0,0 +1,21 @@
+AOS.init({
+duration:1000,
+once:true
+});
+
+new Typed(".typing",{
+
+strings:[
+"Software Developer",
+"IT Professional",
+"Cloud Enthusiast",
+"Cybersecurity Enthusiast",
+"AI & IoT Innovator"
+],
+
+typeSpeed:70,
+backSpeed:50,
+backDelay:1500,
+loop:true
+
+});
